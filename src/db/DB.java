ab1748a5ae1c90f805d7c4c7410cc5a5f2ec1c8c
Rafael Ribeiro Rodrigues <rafaelribeirorodrigues84@gmail.com>
2026-06-10 21:25:06 -0300

    Conexão com o jdbc

---
+++ src/db/DB.java
@@ -0,0 +1,55 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+
+	public static Connection conn = null;
+
+	// Iniciar conexão
+	public static Connection getConnection() {
+
+		if (conn == null) {
+			try {
+				Properties props = new Properties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+
+		return conn;
+	}
+
+	// Fechar Conexão
+
+	public static void closeConnection() {
+
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+
+	// Ler o arquivo db.properties.
+	private static Properties loadProperties() {
+
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+
+		} catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module ProjetoDao {
+	requires java.sql;
+}
