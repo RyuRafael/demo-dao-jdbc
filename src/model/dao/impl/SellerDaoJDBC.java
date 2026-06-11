854c6f2cb5abf0d07ec6c5816a5f714cb6028b38
Rafael Ribeiro Rodrigues <rafaelribeirorodrigues84@gmail.com>
2026-06-11 16:31:06 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,33 @@
+package model.dao.impl;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao {
+    @Override
+    public void insert(Seller obj) {
+
+    }
+
+    @Override
+    public void update(Seller obj) {
+
+    }
+
+    @Override
+    public void deleterById(Integer id) {
+
+    }
+
+    @Override
+    public void findById(Integer id) {
+
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return List.of();
+    }
+}
